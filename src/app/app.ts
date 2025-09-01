5edcf033993f563fedbe358630770d3484c02190
NicolasBELLANGER <nicobell93220@gmail.com>
2025-09-01 12:17:52 +0200

    test: test de prettierrc, lint, husky

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-todo-list');
-  add(n:any){
-    console.log(n);
-  }
 }
